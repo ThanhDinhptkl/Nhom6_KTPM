d27dd620d3aeb641eb92e5161f685e8a67323be1
PhamHuuThuan <92664025+PhamHuuThuan@users.noreply.github.com>
2025-04-23 11:29:20 +0700

    fix error CORS

--- BackEnd/CustomerService/src/main/java/com/tour/customerservice/controller/CircuitBreakerController.java
+++ BackEnd/CustomerService/src/main/java/com/tour/customerservice/controller/CircuitBreakerController.java
@@ -4,15 +4,13 @@ import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/circuit-breaker")
 public class CircuitBreakerController {
 
